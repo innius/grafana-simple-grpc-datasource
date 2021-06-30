4cdf3aeb6ff5e3f53c5245a896dc2051159ab0f6
Ron van der Wijngaard <rvdwijngaard@innius.com>
2021-06-30 12:30:50 +0200

    fix: linting errors

--- src/QueryEditor.tsx
+++ src/QueryEditor.tsx
@@ -1,105 +1,105 @@
 import defaults from 'lodash/defaults';
 
-import React, {PureComponent} from 'react';
-import {InlineField, InlineFormLabel, LegacyForms} from '@grafana/ui';
-import {QueryEditorProps, Registry, SelectableValue} from '@grafana/data';
-import {DataSource} from './datasource';
-import {AggregateType, defaultQuery, MyDataSourceOptions, MyQuery, QueryType} from './types';
-import {changeQueryType, QueryTypeInfo, queryTypeInfos} from 'queryInfo';
+import React, { PureComponent } from 'react';
+import { InlineField, InlineFormLabel, LegacyForms } from '@grafana/ui';
+import { QueryEditorProps, Registry, SelectableValue } from '@grafana/data';
+import { DataSource } from './datasource';
+import { AggregateType, defaultQuery, MyDataSourceOptions, MyQuery, QueryType } from './types';
+import { changeQueryType, QueryTypeInfo, queryTypeInfos } from 'queryInfo';
 import DimensionSettings from './QueryDimensions';
 
-const {Select, AsyncSelect} = LegacyForms;
+const { Select, AsyncSelect } = LegacyForms;
 
 type Props = QueryEditorProps<DataSource, MyQuery, MyDataSourceOptions>;
 
 export const aggReg = new Registry(() => [
-    {id: AggregateType.AVERAGE, name: 'Average'},
-    // { id: AggregateType.COUNT, name: 'Count', isValid: AnyTypeOK },
-    {id: AggregateType.MAXIMUM, name: 'Max'},
-    {id: AggregateType.MINIMUM, name: 'Min'},
-    // { id: AggregateType.SUM, name: 'Sum', isValid: OnlyNumbers },
-    // { id: AggregateType.STANDARD_DEVIATION, name: 'Stddev', description: 'Standard Deviation', isValid: OnlyNumbers },
+  { id: AggregateType.AVERAGE, name: 'Average' },
+  // { id: AggregateType.COUNT, name: 'Count', isValid: AnyTypeOK },
+  { id: AggregateType.MAXIMUM, name: 'Max' },
+  { id: AggregateType.MINIMUM, name: 'Min' },
+  // { id: AggregateType.SUM, name: 'Sum', isValid: OnlyNumbers },
+  // { id: AggregateType.STANDARD_DEVIATION, name: 'Stddev', description: 'Standard Deviation', isValid: OnlyNumbers },
 ]);
 
 export class QueryEditor extends PureComponent<Props> {
-    constructor(props: Props) {
-        super(props);
-    }
+  constructor(props: Props) {
+    super(props);
+  }
 
-    onQueryTypeChange = (sel: SelectableValue<QueryType>) => {
-        const {onChange, query, onRunQuery} = this.props;
-        onChange(changeQueryType(query, sel as QueryTypeInfo));
-        onRunQuery();
-    };
+  onQueryTypeChange = (sel: SelectableValue<QueryType>) => {
+    const { onChange, query, onRunQuery } = this.props;
+    onChange(changeQueryType(query, sel as QueryTypeInfo));
+    onRunQuery();
+  };
 
-    onMetricChange = (sel: SelectableValue<string>) => {
-        const {onChange, query, onRunQuery} = this.props;
-        onChange({...query, metricName: sel.label, metricId: sel.value});
-        onRunQuery();
-    };
+  onMetricChange = (sel: SelectableValue<string>) => {
+    const { onChange, query, onRunQuery } = this.props;
+    onChange({ ...query, metricName: sel.label, metricId: sel.value });
+    onRunQuery();
+  };
 
-    onAggregateChange = (item: SelectableValue<AggregateType>) => {
-        const {onChange, query, onRunQuery} = this.props;
-        onChange({...query, aggregateType: item && item.value});
-        onRunQuery();
-    };
+  onAggregateChange = (item: SelectableValue<AggregateType>) => {
+    const { onChange, query, onRunQuery } = this.props;
+    onChange({ ...query, aggregateType: item && item.value });
+    onRunQuery();
+  };
 
-    loadMetrics = (value: string): Promise<Array<SelectableValue<string>>> => {
-        const {datasource} = this.props;
-        const {dimensions} = this.props.query;
-        return datasource.listMetrics(dimensions || [], value);
-    };
+  loadMetrics = (value: string): Promise<Array<SelectableValue<string>>> => {
+    const { datasource } = this.props;
+    const { dimensions } = this.props.query;
+    return datasource.listMetrics(dimensions || [], value);
+  };
 
-    render() {
-        const query = defaults(this.props.query, defaultQuery);
-        const queryTooltip = '';
-        const currentQueryType = queryTypeInfos.find(v => v.value === query.queryType);
-        const select = aggReg.selectOptions([query.aggregateType || ''])
-        // AsyncSelect is not perfect yet, see https://github.com/JedWatson/react-select/issues/1879 for an alternative solution
-        return (
-            <div className="gf-form-group">
-                <>
-                    <div className="gf-form">
-                        <InlineField label="Query type" labelWidth={14} grow={true} tooltip={queryTooltip}>
-                            <Select
-                                options={queryTypeInfos}
-                                value={currentQueryType}
-                                onChange={this.onQueryTypeChange}
-                                placeholder="Select query type"
-                                menuPlacement="bottom"
-                            />
-                        </InlineField>
-                    </div>
-                    <DimensionSettings {...this.props} />
-                    <>
-                        <div className={'gf-form'}>
-                            <InlineFormLabel width={5} tooltip={'start typing to query for metrics'}>
-                                Metric
-                            </InlineFormLabel>
-                            <AsyncSelect
-                                width={12}
-                                defaultOptions={false}
-                                value={{label: query.metricName, value: query.metricId}}
-                                loadOptions={this.loadMetrics}
-                                noOptionsMessage={() => 'type to search for metrics'}
-                                onChange={this.onMetricChange}
-                            />
-                        </div>
-
-                        <div
-                            className={'gf-form'}
-                            hidden={currentQueryType ? currentQueryType.value !== QueryType.GetMetricAggregate : true}
-                        >
-                            <InlineFormLabel width={5}>Aggregate</InlineFormLabel>
-                            <Select
-                                value={select.current} //TODO: improve this
-                                options={select.options as any}
-                                onChange={this.onAggregateChange}
-                            />
-                        </div>
-                    </>
-                </>
+  render() {
+    const query = defaults(this.props.query, defaultQuery);
+    const queryTooltip = '';
+    const currentQueryType = queryTypeInfos.find(v => v.value === query.queryType);
+    const select = aggReg.selectOptions([query.aggregateType || '']);
+    // AsyncSelect is not perfect yet, see https://github.com/JedWatson/react-select/issues/1879 for an alternative solution
+    return (
+      <div className="gf-form-group">
+        <>
+          <div className="gf-form">
+            <InlineField label="Query type" labelWidth={14} grow={true} tooltip={queryTooltip}>
+              <Select
+                options={queryTypeInfos}
+                value={currentQueryType}
+                onChange={this.onQueryTypeChange}
+                placeholder="Select query type"
+                menuPlacement="bottom"
+              />
+            </InlineField>
+          </div>
+          <DimensionSettings {...this.props} />
+          <>
+            <div className={'gf-form'}>
+              <InlineFormLabel width={5} tooltip={'start typing to query for metrics'}>
+                Metric
+              </InlineFormLabel>
+              <AsyncSelect
+                width={12}
+                defaultOptions={false}
+                value={{ label: query.metricName, value: query.metricId }}
+                loadOptions={this.loadMetrics}
+                noOptionsMessage={() => 'type to search for metrics'}
+                onChange={this.onMetricChange}
+              />
             </div>
-        );
-    }
+
+            <div
+              className={'gf-form'}
+              hidden={currentQueryType ? currentQueryType.value !== QueryType.GetMetricAggregate : true}
+            >
+              <InlineFormLabel width={5}>Aggregate</InlineFormLabel>
+              <Select
+                value={select.current} //TODO: improve this
+                options={select.options as any}
+                onChange={this.onAggregateChange}
+              />
+            </div>
+          </>
+        </>
+      </div>
+    );
+  }
 }
--- src/types.ts
+++ src/types.ts
@@ -1,91 +1,91 @@
-import {DataQuery, DataSourceJsonData} from '@grafana/data';
+import { DataQuery, DataSourceJsonData } from '@grafana/data';
 
 export enum QueryType {
-    ListDimensionKeys = 'ListDimensionKeys',
-    ListDimensionValues = 'ListDimensionValues',
-    ListMetrics = 'ListMetrics',
-    GetMetricValue = 'GetMetricValue',
-    GetMetricHistory = 'GetMetricHistory',
-    GetMetricAggregate = 'GetMetricAggregate',
+  ListDimensionKeys = 'ListDimensionKeys',
+  ListDimensionValues = 'ListDimensionValues',
+  ListMetrics = 'ListMetrics',
+  GetMetricValue = 'GetMetricValue',
+  GetMetricHistory = 'GetMetricHistory',
+  GetMetricAggregate = 'GetMetricAggregate',
 }
 
 export enum AggregateType {
-    AVERAGE = 'AVERAGE',
-    // COUNT = 'COUNT',
-    MAXIMUM = 'MAXIMUM',
-    MINIMUM = 'MINIMUM',
-    // SUM = 'SUM',
-    // STANDARD_DEVIATION = 'STANDARD_DEVIATION',
+  AVERAGE = 'AVERAGE',
+  // COUNT = 'COUNT',
+  MAXIMUM = 'MAXIMUM',
+  MINIMUM = 'MINIMUM',
+  // SUM = 'SUM',
+  // STANDARD_DEVIATION = 'STANDARD_DEVIATION',
 }
 
 export function isMetricQuery(queryType: QueryType): boolean {
-    return (
-        queryType === QueryType.GetMetricValue ||
-        queryType === QueryType.GetMetricHistory ||
-        queryType === QueryType.GetMetricAggregate
-    );
+  return (
+    queryType === QueryType.GetMetricValue ||
+    queryType === QueryType.GetMetricHistory ||
+    queryType === QueryType.GetMetricAggregate
+  );
 }
 
 export interface MyQuery extends DataQuery {
-    queryType: QueryType;
-    dimensions?: Dimensions;
-    metricName?: string;
-    metricId?: string;
-    aggregateType?: AggregateType;
+  queryType: QueryType;
+  dimensions?: Dimensions;
+  metricName?: string;
+  metricId?: string;
+  aggregateType?: AggregateType;
 }
 
 export interface Dimension {
-    id: string;
-    key: string;
-    value: string;
+  id: string;
+  key: string;
+  value: string;
 }
 
 export type Dimensions = Dimension[];
 
 export const defaultQuery: Partial<MyQuery> = {
-    dimensions: [],
+  dimensions: [],
 };
 
 /**
  * These are options configured for each DataSource instance
  */
 export interface MyDataSourceOptions extends DataSourceJsonData {
-    endpoint?: string;
-    apikey_authentication_enabled: boolean;
+  endpoint?: string;
+  apikey_authentication_enabled: boolean;
 }
 
 /**
  * Value that is used in the backend, but never sent over HTTP to the frontend
  */
 export interface MySecureJsonData {
-    apiKey?: string;
+  apiKey?: string;
 }
 
 export interface GetMetricValueQuery extends MyQuery {
-    queryType: QueryType.GetMetricValue;
+  queryType: QueryType.GetMetricValue;
 }
 
 export interface GetMetricHistoryQuery extends MyQuery {
-    queryType: QueryType.GetMetricHistory;
+  queryType: QueryType.GetMetricHistory;
 }
 
 export interface GetMetricAggregateQuery extends MyQuery {
-    queryType: QueryType.GetMetricAggregate;
+  queryType: QueryType.GetMetricAggregate;
 }
 
 export interface ListDimensionsQuery extends MyQuery {
-    queryType: QueryType.ListDimensionKeys;
-    filter: string;
+  queryType: QueryType.ListDimensionKeys;
+  filter: string;
 }
 
 export interface ListDimensionValuesQuery extends MyQuery {
-    queryType: QueryType.ListDimensionValues;
-    dimensionKey: string;
-    filter: string;
+  queryType: QueryType.ListDimensionValues;
+  dimensionKey: string;
+  filter: string;
 }
 
 export interface ListMetricsQuery extends MyQuery {
-    queryType: QueryType.ListMetrics;
-    dimensions: Dimensions;
-    filter: string;
+  queryType: QueryType.ListMetrics;
+  dimensions: Dimensions;
+  filter: string;
 }
